d6fba7767810f88355975dd93d297b632b8dc5d7
Nikhil Idiculla <nikhilidiculla@gmail.com>
2024-12-22 19:01:04 -0800

    Flush output on each datum (#4)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod printer;
 mod reader;
 mod sexp;
 
+use std::io::Write;
+
 use parser::*;
 use printer::*;
 use reader::*;
@@ -16,6 +18,7 @@ fn main_inner() -> std::result::Result<(), Box<dyn std::error::Error>> {
 				let output = parse_form(s);
 				print_sexp(output);
 				println!();
+				std::io::stdout().flush()?;
 			}
 			None => {
 				break;
